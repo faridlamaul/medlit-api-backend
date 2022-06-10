107376c946f5da4ca1203e8151b1e12080f3dd6e
faridlamaul <lamaulfarid71@gmail.com>
2022-06-10 17:22:59 +0700

    add Setup model

---
+++ api/models/Setup.go
@@ -0,0 +1,41 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDatabase() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	DbHost := os.Getenv("DB_HOST")
+	Dbdriver := os.Getenv("DB_DRIVER")
+	DbUser := os.Getenv("DB_USER")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbName := os.Getenv("DB_NAME")
+	DbPort := os.Getenv("DB_PORT")
+
+	DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
+
+	DB, err = gorm.Open(mysql.Open(DBURL), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Cannot connect to database", Dbdriver)
+		log.Fatal("connection error:", err)
+	} else {
+		fmt.Println("Connected to database", Dbdriver)
+	}
+
+	DB.AutoMigrate(&User{}, &Medicine{})
+}
