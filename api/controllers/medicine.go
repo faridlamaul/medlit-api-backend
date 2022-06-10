581cb080d0b10992ea24ceffc7405844daf95c0e
faridlamaul <lamaulfarid71@gmail.com>
2022-06-10 17:27:23 +0700

    add medicine controller to handle all request from client

---
+++ api/controllers/medicine.go
@@ -0,0 +1,100 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/faridlamaul/medlit-api-backend/api/models"
+)
+
+type MedicineInput struct {
+	GenericName string `json:"generic_name" binding:"required"`
+	PhotoURL string `json:"photo_url" binding:"required"`
+	Purpose string `json:"purpose" binding:"required"`
+	SideEffects string `json:"side_effects" binding:"required"`
+	Contraindication string `json:"contraindication" binding:"required"`
+}
+
+func AddMedicine(c *gin.Context) {
+	var input MedicineInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "true",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	medicine := models.Medicine{}
+	medicine.GenericName = input.GenericName
+	medicine.PhotoURL = input.PhotoURL
+	medicine.Purpose = input.Purpose
+	medicine.SideEffects = input.SideEffects
+	medicine.Contraindication = input.Contraindication
+
+	_, err := medicine.CreateMedicine()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "true",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"error": "false",
+		"message": "Medicine created",
+	})
+}
+
+func GetAllMedicine(c *gin.Context) {
+	medicines := models.GetAllMedicine()
+
+	c.JSON(http.StatusOK, gin.H{
+		"error": "false",
+		"message": "Medicine list",
+		"medicineList": medicines,
+	})
+}
+
+func GetMedicineByID(c *gin.Context) {
+	id := c.Param("id")
+
+	medicine, err := models.GetMedicineByID(id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "true",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"error": "false",
+		"message": "Medicine found",
+		"medicine": medicine,
+	})
+}
+
+func GetMedicineByQuery(c *gin.Context) {
+	generic_name := c.Query("generic_name")
+
+	medicines, err := models.GetMedicineByQuery(generic_name)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "true",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"error": "false",
+		"message": "Medicine found",
+		"medicineList": medicines,
+	})
+}
