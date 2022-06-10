4530009319a87f372083df698b2235a1958a6425
faridlamaul <lamaulfarid71@gmail.com>
2022-06-10 17:26:13 +0700

    add Medicine model

---
+++ api/models/Medicine.go
@@ -0,0 +1,43 @@
+package models
+
+import "time"
+
+type Medicine struct {
+	ID uint32 `gorm:"primary_key;auto_increment" json:"id"`
+	GenericName string `gorm:"size:255;not null" json:"generic_name"`
+	PhotoURL string `gorm:"size:255;not null" json:"photo_url"`
+	Purpose string `gorm:"size:255;not null" json:"purpose"`
+	SideEffects string `gorm:"size:255;not null" json:"side_effects"`
+	Contraindication string `gorm:"size:255;not null" json:"contraindication"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func (m *Medicine) CreateMedicine() (*Medicine, error) {
+	err := DB.Create(&m).Error
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func GetAllMedicine() []Medicine {
+	var medicines []Medicine
+	DB.Find(&medicines)
+	return medicines
+}
+
+func GetMedicineByID(id string) (*Medicine, error) {
+	m := &Medicine{}
+	err := DB.First(&m, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func GetMedicineByQuery(query string) ([]Medicine, error) {
+	var medicines []Medicine
+	DB.Where("generic_name LIKE ?", "%"+query+"%").Find(&medicines)
+	return medicines, nil
+}
